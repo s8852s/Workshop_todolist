b1601509f31c47f53868963a39e6bd9facb8384f
s8852s <s8852s@hotmail.com.tw>
2020-12-08 11:49:33 +0800

    add_task

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,13 @@
 // TO DO
+//add_task
+document.addEventListener("DOMContentLoaded", function(){
+  //add_task
+  document.querySelector(".addBtn").addEventListener("click", function(){
+    let task = document.getElementById("input").value
+    if ( task != null ){
+    let li = document.createElement("li");
+    li.innerHTML = (`${task}<span class='close'>x</span>`)
+    document.querySelector("ul").appendChild(li)
+    }
+  })
+})
